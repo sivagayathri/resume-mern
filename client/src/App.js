17d976af1de37500d530aafd086db82d71757c59
sivagayathri <shivagayathrik2000@gmail.com>
2023-05-29 14:10:15 +0530

    resume

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Resume from "./components/Resume";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
